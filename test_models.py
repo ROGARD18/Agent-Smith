627d8566a42de1ea9b29083f6bca4f7078b496b6
Antoine Rogard <a18ntoinecbr@icloud.com>
2026-09-12 22:11:22 +0200

    save

---
+++ test_models.py
@@ -0,0 +1,5 @@
+import os, requests
+key = os.environ.get("GEMINI_API_KEY")
+r = requests.get(f"https://generativelanguage.googleapis.com/v1beta/models?key={key}")
+for m in r.json().get("models", []):
+    print(m["name"])
